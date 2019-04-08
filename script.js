de0a39d0557aa89558680e9df11a7c0346684bcd
Aleksandr <44066055+s0885466@users.noreply.github.com>
2019-04-08 13:43:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,35 +6,36 @@
 *
 * */
 
-function shoot() {
-
-    console.log('Вы бросили монетку...');
-
-    let promise = new Promise(function (resolve, reject) {
-        setTimeout(function () {
-            Math.random() > 0.5 ? resolve() : reject();
-        }, 3000);
-    });
-
-    return promise;
+function getTicket(){
+	console.log('Я хочу купить билет');
+	
+	var promise = new Promise(function(resolve, reject) {
+	setTimeout(()=>(Math.random() < 0.8)? resolve(): reject(), 2000 );
+	});
+	
+	return promise;
 }
 
-function dropEagle(){
-    console.log('Выпал орел');
+function getOk(){
+	console.log('Все отлично, у нас есть билеты')
+	//setTimeout(()=>console.log('Все отлично, у нас есть билеты'),2000);
+	setTimeout(()=>getMoney(),2000);
 }
 
-function dropTail(){
-    console.log('Выпала решка');
+function getMoney(){
+	console.log('С вас 500 рублей');
 }
 
 
-/*shoot()
-    .then(() => console.log('Монетка брошена и кажется это орел'))
-    .then(dropEagle)
-    .catch(dropTail)
-    .then(() => console.log('игра окончена'));*/
+function getError(){
+	console.log('К сожалению билеты закончились');
+	setTimeout(()=>console.log('Приходите в следующий раз'),2000);
+	
+}
 
-shoot()
-    .then(() => console.log('Монетка брошена и кажется это орел'))
-    .then(dropEagle, dropTail)
-    .then(() => console.log('игра окончена'));
+
+
+getTicket().then(getOk)
+			.catch(getError);
+			
+
